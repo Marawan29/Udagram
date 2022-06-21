01a88fdf4b92125146719814490f95bfb0d0bdde
Marawan-Sultan <munny13ang@gmil.com>
2022-06-21 05:21:26 +0200

    Hope it works

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -7,17 +7,17 @@ dotenv.config();
 // Are Also needed
 
 export const config = {
-  username: `${process.env.POSTGRES_USERNAME}`,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  port_db: Number(process.env.PORT_DB),
-  host: process.env.POSTGRES_HOST,
+  username: "postgres",
+  password: "12345qwer",
+  database: "udagram",
+  port_db: 54321,
+  host: "udagram.cqb8bmyscp11.us-east-1.rds.amazonaws.com",
   dialect: "postgres",
-  aws_region: process.env.AWS_REGION,
-  aws_profile: process.env.AWS_PROFILE,
-  aws_media_bucket: process.env.AWS_BUCKET,
-  url: process.env.URL,
+  aws_region: "u-east-1",
+  aws_profile: "default",
+  aws_media_bucket: "udauda",
+  url: "http://udauda.s3-website-us-east-1.amazonaws.com",
   jwt: {
-    secret: process.env.JWT_SECRET,
+    secret: "Manatarms",
   },
 };
--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-    apiHost: 'http://udauda-env.eba-ctsamvji.us-east-1.elasticbeanstalk.com'
+    apiHost: 'http://udauda-env-1.eba-3a8s5ypx.us-east-1.elasticbeanstalk.com'
 };
 
 /*
